1cae38647798a91eea1be65ba9de9f3de2005392
mickaeltartar <mickael.tartar@hotmail.com>
2024-01-02 15:02:39 +0100

    return sum of the list in float

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" complex types of list of floats """
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ return the sum of the list """
+    return(sum(input_list))
