528f0e9d23db710aa770e575831656d5a77168fa
mickaeltartar <mickael.tartar@hotmail.com>
2024-01-02 11:27:10 +0100

    pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Basic annotations """
 
-def add(a : float, b : float) -> float:
+def add(a: float, b: float) -> float:
     """ define a & b in float """
     return (a + b)
