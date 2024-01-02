a4c83745b990d95d6ddf83d6f97493c2c1a97cdc
mickaeltartar <mickael.tartar@hotmail.com>
2024-01-02 15:04:37 +0100

    add comment in second file & pycodestyle for sum of list file

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -6,5 +6,5 @@ import math
 
 
 def floor(n: float) -> int:
-    """ return floor of float """
+    """ return floor of float in int """
     return math.floor(n)
--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -5,4 +5,4 @@ from typing import List
 
 def sum_list(input_list: List[float]) -> float:
     """ return the sum of the list """
-    return(sum(input_list))
+    return (sum(input_list))
