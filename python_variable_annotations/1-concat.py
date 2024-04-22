4d272b7d49faf769127c76abff86106ce85997c3
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-22 09:21:30 +0200

    add new line at the end of the file

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -2,6 +2,6 @@
 """basics annotation"""
 
 
-def concat(str1 :str, str2: str) -> str:
+def concat(str1: str, str2: str) -> str:
     """ concat two string """
-    return(str1 + str2)
+    return (str1 + str2)
