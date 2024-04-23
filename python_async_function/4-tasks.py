ad263b9c0e0117731633b1f2959a26bb3e61252d
mickaeltartar <mickael.tartar@hotmail.com>
2024-04-23 12:35:35 +0200

    transform a function task_wait_n for return list of all delays

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
-"""written and write an async routine"""
+"""transform in new def task and return list of delay"""
 
 import asyncio
 import random
 from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """return list of delay in ascending order"""
 
     delays = []
-    tasks = [wait_random(max_delay) for _ in range(n)]
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
     for task in asyncio.as_completed(tasks):
         delay = await task
         delays.append(delay)
