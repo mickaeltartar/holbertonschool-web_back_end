9f20bf66e83e8a67cb0153ab0c4bc70fddb0a402
mickaeltartar <mickael.tartar@hotmail.com>
2024-05-02 09:45:46 +0200

    indent code

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,6 +4,7 @@
 
 def update_topics(mongo_collection, name, topics):
     """Update"""
-    mongo_collection.update_many({"name": name},
-                                 {'$set': {"topics": topics}}
-                                 )
+    mongo_collection.update_many(
+        {"name": name},
+        {'$set': {"topics": topics}}
+    )
