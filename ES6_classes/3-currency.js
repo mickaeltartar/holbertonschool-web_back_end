5a224d4e79a803e7a4d5868bcbd6e67e153f89ea
mickaeltartar <mickael.tartar@hotmail.com>
2024-05-07 17:08:30 +0200

    add a class currency

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,40 @@
+export default class Currency {
+  constructor(name, code) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = newName;
+    }
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    } else {
+      this._code = newCode;
+    }
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
